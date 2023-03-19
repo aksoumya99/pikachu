509ddc562427515cbbbf3d34b7cd88fc9d88ec58
Soumya Agarwal <soumya@Soumyas-MacBook-Pro.local>
2023-03-19 20:02:32 +0530

    Allowed hosts changed

--- pikachu/pikachu/settings.py
+++ pikachu/pikachu/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--dj5b!objek$5-b*k_bz(i8mb$=iy*79+_##4h2=!s=9v3)g*4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
